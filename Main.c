8d458ba4e9fe139ac9e30a62a8061e97ae24d8ba
Avihay Barnholtz <57184849+AvihayBarn@users.noreply.github.com>
2022-12-14 13:08:12 +0200

    f

--- Main.c
+++ Main.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <unistd.h>
+
 #include <stdlib.h>
-#include <strings.h>
-#include <sys/wait.h>
+
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <errno.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <sys/mman.h>
+
 
 // define for UDS
 #define SOCK_PATH "tpf_unix_sock.server"
