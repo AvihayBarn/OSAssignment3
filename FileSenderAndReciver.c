828def41b45ecb6636c689e0b9d7fd6b93d6ca4c
Avihay Barnholtz <57184849+AvihayBarn@users.noreply.github.com>
2022-12-15 23:05:28 +0200

    update

--- FileSenderAndReciver.c
+++ FileSenderAndReciver.c
@@ -98,7 +98,7 @@ int main(int argc, char* argv[])
          }
          else if(strcmp("UDS_stream", method) == 0)
          {
-             sendUDS_stream();
+             UDS_stream();
          }
          else if(strcmp("MMap", method) == 0)
          {
